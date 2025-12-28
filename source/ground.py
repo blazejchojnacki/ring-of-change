c8e5e81c9da3422d720efcef1071fd7862da938c
blazejchojnacki <blazejchojnacki@outlook.com>
2025-12-29 00:06:00 +0100

    Added root.py and ground.py start files

---
+++ source/ground.py
@@ -0,0 +1,13 @@
+""" This module contains constants and functions common to all modules. """
+
+
+def log():
+    pass
+
+
+def get_calling_module():
+    pass
+
+
+def get_calling_object():
+    pass
---
+++ source/root.py
@@ -0,0 +1,6 @@
+"""This module contains variables that are global for all the modules"""
+
+library = './_LIBRARY'
+archive = './_ARCHIVE'
+games = []
+not_mods = []
